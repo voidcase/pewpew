04527cca5a0050a52ecfb4b1beb6457fcbaed765
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-10-11 11:50:58 +0200

    regressor is now trainable.

--- regressor.py
+++ regressor.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
+from scipy.misc import imresize
 from tensorflow import keras
 from tensorflow.nn import relu
 
-IMAGE_SHAPE = (1000, 1000) #TEMP
 
 def build_model():
     md = keras.models.Sequential()
@@ -22,16 +22,31 @@ def build_model():
     md.add(keras.layers.Flatten())
     md.add(keras.layers.Dense(32, activation=relu))
     md.add(keras.layers.Dense(1, activation=relu))
+    md.compile(
+            optimizer='adam',
+            loss='mean_squared_error',
+            )
+
+
 
 def load_data(path):
     from os import listdir
     filenames = listdir(path)
-    return np.stack([cv2.imread(path+fn, 0) for fn in filenames])
+    return np.stack([imresize(cv2.imread(path+fn, 1), 0.2) for fn in filenames])
+
 
 def gen_bullshit_y_data(length):
     from random import randrange
     return np.array([randrange(0,1000) for i in range(length)])
 
+
+def trained_model():
+    x = load_data('data/')
+    y = gen_bullshit_y_data(x.shape[0])
+    md = build_model()
+    md.fit(x,y)
+    return md
+
+
 if __name__ == '__main__':
-    data = load_data('./data/')
-    assert False
+    trained_model()
