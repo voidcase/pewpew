5d129dd32c35d112f15b9d42d146702440a639d8
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-10-22 11:40:36 +0200

    datagen ready for real mxcube

--- datagen.py
+++ datagen.py
@@ -4,23 +4,45 @@ import numpy as np
 from time import time
 import sqlite3 as sql
 
-
-def get_stream() -> requests.Response:
-    cookies = {
-        'session': '4eb6807d-ed71-46d8-a7d5-5e1ea0c0ca12',
-    }
-    headers = {
-        'Host': 'w-v-kitslab-mxcube-1:8081',
+cookies = {
+    'session': '4eb6807d-ed71-46d8-a7d5-5e1ea0c0ca12',
+}
+headers = {
+    'Host': 'w-v-kitslab-mxcube-1:8081',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Upgrade-Insecure-Requests': '1',
+    'DNT': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0',
+}
+real_cookies = {
+    'session': '624f629e-b4e8-419f-9370-ab3dadce37c8',
+}
+real_headers = {
+        'Host': 'http://b-v-biomax-web-0:8081',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
         'DNT': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0',
-    }
+        }
+
+
+def setblacklight(mode):
+    requests.put(
+            'http://b-v-biomax-web-0:8081/mxcube/api/v0.1/sampleview/backlight{}'.format('on' if mode else 'off'),
+            cookies=real_cookies,
+            headers=real_headers,
+            timeout=2,
+            )
+
+
+def get_stream() -> requests.Response:
     return requests.get(
-            'http://w-v-kitslab-mxcube-1:8081/mxcube/api/v0.1/sampleview/camera/subscribe',
-            headers=headers,
-            cookies=cookies,
+            'http://b-v-biomax-web-0:8081/mxcube/api/v0.1/sampleview/camera/subscribe',
+            headers=real_headers,
+            cookies=real_cookies,
             stream=True,
             timeout=2
             )
@@ -81,18 +103,16 @@ def create_db(path):
 
 def save_buffer(buf, path):
     from uuid import uuid4
-    print('saving buffer')
+    print('saving buffer to disk at {}'.format(time()))
     db = create_db(path)
     if len(buf) == 0:
         return
     query = 'insert into images (uuid, timestamp) values '
     for img, timestamp in buf:
         image_id = uuid4()
-        # with open(f'{path}/images/{image_id}.jpg', 'wb') as imfile:
-        #     imfile.write(img)
         cv2.imwrite(f'{path}/images/{image_id}.jpg', img)
         query += f'("{image_id}", "{timestamp}"),'
-    query = query[:-1]  # remove last ','
+    query = query[:-1]  # remove last ',' from query
     query += ';'
     db.execute(query)
     db.commit()
@@ -105,9 +125,11 @@ if __name__ == '__main__':
     gen = img_stream()
     prev = time()
     print('watching camera stream...')
-    for iteration in range(5):
+    last_save = time()
+    while True:
         i, t = next(gen)
         camera_buffer.append((i, t))
-        print(t - prev)
-        prev = t
-    save_buffer(camera_buffer, PATH)
+        if t - last_save > 60:
+            save_buffer(camera_buffer, PATH)
+            camera_buffer = []
+            last_save = t
