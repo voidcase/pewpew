4d47cd99b8932822ecf904365e942b2a02cb5e24
Jonathan Schurmann <jonathan.schurmann@gmail.com>
2018-10-25 13:40:04 +0200

    Use session to get new cookie every time

--- camera_watcher.py
+++ camera_watcher.py
@@ -5,76 +5,45 @@ import sys
 import sqlite3 as sql
 from time import time
 
-REAL = True
-# REAL = False
+# REAL = True
+REAL = False
 
-API_PATH = '/mxcube/api/v0.1'
+URL = "http://b-v-biomax-web-0:8081" if REAL else "http://w-v-kitslab-mxcube-1:8081"
+API_PATH = f"{URL}/mxcube/api/v0.1"
 
-mock_cookies = {
-    'session': '4eb6807d-ed71-46d8-a7d5-5e1ea0c0ca12',
-}
-
-mock_headers = {
-    'Host': 'http://w-v-kitslab-mxcube-1:8081',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Upgrade-Insecure-Requests': '1',
-    'DNT': '1',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0',
-}
-
-real_cookies = {
-    'session': '624f629e-b4e8-419f-9370-ab3dadce37c8',
-}
-
-real_headers = {
-        'Host': 'http://b-v-biomax-web-0:8081',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'DNT': '1',
-        }
+auth = dict(proposal="idtest000", password="")
+# login = dict(proposal='', password='')
 
 
-APIURL = (real_headers if REAL else mock_headers)['Host'] + API_PATH
+def setup_session() -> requests.Session:
+    s = requests.Session()
+    res = s.post(f"{API_PATH}/login", json=auth)
+    if not res.ok:
+        raise Exception(f"Error {res.status_code}: {res.reason}")
+    return s
 
 
-def reqstuff():
-    if REAL:
-        return dict(cookies=real_cookies, headers=real_headers, timeout=2)
-    else:
-        return dict(cookies=mock_cookies, headers=mock_headers, timeout=2)
-
-
-def setblacklight(mode):
-    requests.put(
-            '{}/sampleview/backlight{}'.format(APIURL, 'on' if mode else 'off'),
-            timeout=2,
-            **reqstuff(),
-            )
+def setblacklight(mode: bool):
+    res = s.put(f"{API_PATH}/sampleview/backlight{'on' if mode else 'off'}", timeout=2)
+    if not res.ok:
+        raise Exception(f"Error {res.status_code}: {res.reason}")
 
 
 def get_stream() -> requests.Response:
-    return requests.get(
-            f'{APIURL}/sampleview/camera/subscribe',
-            stream=True,
-            **reqstuff(),
-            )
+    return s.get(f"{API_PATH}/sampleview/camera/subscribe", stream=True)
 
 
 def img_stream():
     stream = get_stream()
-    if(stream.status_code == 200):
+    if stream.status_code == 200:
         img_buffer = bytes()
         for chunk in stream.iter_content(chunk_size=64):
             img_buffer += chunk
-            start = img_buffer.find(b'\xff\xd8')
-            end = img_buffer.find(b'\xff\xd9')
+            start = img_buffer.find(b"\xff\xd8")
+            end = img_buffer.find(b"\xff\xd9")
             if start != -1 and end != -1:
-                jpg = img_buffer[start:end+2]
-                img_buffer = img_buffer[end+2:]
+                jpg = img_buffer[start : end + 2]
+                img_buffer = img_buffer[end + 2 :]
                 # i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 t = time()
                 # yield (i, t)
@@ -84,7 +53,7 @@ def img_stream():
 
 
 def closest_img(datapath, frame_time):
-    conn = sql.connect(f'{datapath}/meta.db')
+    conn = sql.connect(f"{datapath}/meta.db")
     query = """
         SELECT uuid, timestamp
         FROM images
@@ -93,10 +62,10 @@ def closest_img(datapath, frame_time):
         """
     res = conn.execute(query, frame_time)
     if len(res) < 1:
-        print('db is empty, get some images first.', file=sys.stderr)
+        print("db is empty, get some images first.", file=sys.stderr)
         sys.exit(1)
     uuid, timestamp = list(res)[0]
-    impath = f'{datapath}/img/{uuid}.jpg'
+    impath = f"{datapath}/img/{uuid}.jpg"
     img = cv2.imread(impath)
     if not img:
         print(f'file "{impath}" could not be read for some reason.', file=sys.stderr)
@@ -105,31 +74,33 @@ def closest_img(datapath, frame_time):
 
 
 def create_db(path):
-    conn = sql.connect(f'{path}/meta.db')
+    conn = sql.connect(f"{path}/meta.db")
     conn.execute(
         """
         CREATE TABLE IF NOT EXISTS images (
             uuid VARCHAR(100) UNIQUE NOT NULL,
             timestamp INTEGER NOT NULL
         );
-        """)
+        """
+    )
     return conn
 
 
 def save_buffer(buf, path):
     from uuid import uuid4
-    print('saving buffer to disk at {}'.format(time()))
+
+    print("saving buffer to disk at {}".format(time()))
     db = create_db(path)
     if len(buf) == 0:
         return
-    query = 'insert into images (uuid, timestamp) values '
+    query = "insert into images (uuid, timestamp) values "
     for jpg, timestamp in buf:
         image_id = uuid4()
         i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-        cv2.imwrite(f'{path}/images/{image_id}.jpg', i)
+        cv2.imwrite(f"{path}/images/{image_id}.jpg", i)
         query += f'("{image_id}", "{timestamp}"),'
     query = query[:-1]  # remove last ',' from query
-    query += ';'
+    query += ";"
     db.execute(query)
     db.commit()
     db.close()
@@ -137,16 +108,17 @@ def save_buffer(buf, path):
 
 def eval_fps(buf):
     delays = [b[1] - a[1] for a, b in zip(buf[:-1], buf[1:])]
-    avg_delay = sum(delays)/len(delays)
-    return 1/avg_delay
+    avg_delay = sum(delays) / len(delays)
+    return 1 / avg_delay
 
 
-if __name__ == '__main__':
-    PATH = 'data/cam'
+if __name__ == "__main__":
+    PATH = "data/cam"
     camera_buffer = []
+    s = setup_session()
     gen = img_stream()
     prev = time()
-    print('watching camera stream...')
+    print("watching camera stream...")
     last_save = time()
     while True:
         i, t = next(gen)
@@ -154,7 +126,7 @@ if __name__ == '__main__':
         if not REAL:
             print(t)
         if t - last_save > 30:
-            print('fps:{}'.format(eval_fps(camera_buffer)))
+            print("fps:{}".format(eval_fps(camera_buffer)))
             save_buffer(camera_buffer, PATH)
             camera_buffer = []
             last_save = t
