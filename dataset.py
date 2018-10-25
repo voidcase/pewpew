b7d9c2bf5f19e835aab9202492360aaa19151706
Jonathan Schurmann <jonathan.schurmann@gmail.com>
2018-10-25 14:40:10 +0200

    fix numpy import

--- dataset.py
+++ dataset.py
@@ -5,18 +5,22 @@ import sqlite3 as sql
 
 def get_timestamps(h5filepath: str, num_frames) -> list:
     from datetime import datetime
-    DATEPATH = 'entry/instrument/detector/detectorSpecific/data_collection_date'
+
+    DATEPATH = "entry/instrument/detector/detectorSpecific/data_collection_date"
     f = h5.File(h5filepath)
     try:
         datestr = f[DATEPATH].value
     except KeyError:
-        print(f'file "{h5filepath}" does not have dataset "{DATEPATH}, exiting."', file=sys.stderr)
+        print(
+            f'file "{h5filepath}" does not have dataset "{DATEPATH}, exiting."',
+            file=sys.stderr,
+        )
         sys.exit(1)
-    start_time = datetime.strptime(datestr.decode(), '%Y-%m-%dT%H:%M:%S.%f')
-    exposure_time = f['entry/instrument/detector/frame_time'].value
-    readout_time = f['entry/instrument/detector/detector_readout_time'].value
+    start_time = datetime.strptime(datestr.decode(), "%Y-%m-%dT%H:%M:%S.%f")
+    exposure_time = f["entry/instrument/detector/frame_time"].value
+    readout_time = f["entry/instrument/detector/detector_readout_time"].value
     frame_time = exposure_time + readout_time
-    return [start_time.timestamp() + frame_time*i for i in range(num_frames)]
+    return [start_time.timestamp() + frame_time * i for i in range(num_frames)]
 
 
 def to_cbfs(path: str, h5filename: str) -> int:
@@ -33,36 +37,39 @@ def extract_ys(h5file: str):
 
 def compile_dataset(path):
     from os import listdir
+
     data = list()
-    for h5file in listdir(f'{path}/h5/'):
+    for h5file in listdir(f"{path}/h5/"):
         num_frames = to_cbfs(path, h5file)
-        timestamps = get_timestamps(f'{path}/h5/{h5file}', num_frames)
+        timestamps = get_timestamps(f"{path}/h5/{h5file}", num_frames)
         ys = extract_ys(h5file)
         uuids = [closest_img(t) for t in timestamps]
         data += list(zip(uuids, ys))
     # save to db
     conn = database(path)
-    query = 'INSERT INTO dataset (uuid, y) VALUES '
-    query += ','.join([f'("{uuid}", {y})' for uuid, y in data]) + ';'
+    query = "INSERT INTO dataset (uuid, y) VALUES "
+    query += ",".join([f'("{uuid}", {y})' for uuid, y in data]) + ";"
     conn.execute(query)
 
 
 def database(path):
-    conn = sql.connect(f'{path}/meta.db')
+    conn = sql.connect(f"{path}/meta.db")
     conn.execute(
         """
         CREATE TABLE IF NOT EXISTS images (
             uuid VARCHAR(100) UNIQUE NOT NULL,
             timestamp REAL NOT NULL
         );
-        """)
+        """
+    )
     conn.execute(
         """
         CREATE TABLE IF NOT EXISTS dataset (
             uuid VARCHAR(100) UNIQUE NOT NULL,
             y INTEGER NOT NULL
         );
-        """)
+        """
+    )
     return conn
 
 
@@ -70,26 +77,27 @@ def save_buffer(buf, path):
     from uuid import uuid4
     from time import time
     import cv2
-    import np
-    print('saving buffer to disk at {}'.format(time()))
+    import numpy as np
+
+    print("saving buffer to disk at {}".format(time()))
     db = database(path)
     if len(buf) == 0:
         return
-    query = 'insert into images (uuid, timestamp) values '
+    query = "insert into images (uuid, timestamp) values "
     for jpg, timestamp in buf:
         image_id = uuid4()
         i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-        cv2.imwrite(f'{path}/images/{image_id}.jpg', i)
+        cv2.imwrite(f"{path}/images/{image_id}.jpg", i)
         query += f'("{image_id}", "{timestamp}"),'
     query = query[:-1]  # remove last ',' from query
-    query += ';'
+    query += ";"
     db.execute(query)
     db.commit()
     db.close()
 
 
 def closest_img(datapath, frame_time):
-    conn = sql.connect(f'{datapath}/meta.db')
+    conn = sql.connect(f"{datapath}/meta.db")
     query = """
         SELECT uuid, timestamp
         FROM images
@@ -98,7 +106,7 @@ def closest_img(datapath, frame_time):
         """
     res = conn.execute(query, frame_time)
     if len(res) < 1:
-        print('db is empty, get some images first.', file=sys.stderr)
+        print("db is empty, get some images first.", file=sys.stderr)
         sys.exit(1)
     uuid, timestamp = list(res)[0]
     return uuid, timestamp
