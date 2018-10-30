2a6e7268caeedbc6bd53d8821a99d1bf580b4473
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-10-30 13:03:45 +0100

    Actual unit-tests with pytest

--- testing.py
+++ testing.py
@@ -1,24 +1,29 @@
 import dataset as ds
 
 
-def test_compile():
-    print('compiling dataset')
-    ds.compile_dataset()
-    print('done!')
+# def test_compile():
+#     print('compiling dataset')
+#     ds.compile_dataset()
+#     print('done!')
 
 
-def test_extract_ys():
-    path = ds.cfg.PATH_DIR_H5 / '1149_2-Lysozyme_5_master.h5'
-    ys = ds.extract_ys(path)
-    print(ys)
+# def test_extract_ys():
+#     path = ds.cfg.PATH_DIR_H5 / '1149_2-Lysozyme_5_master.h5'
+#     ys = ds.extract_ys(path)
+#     print(ys)
 
 
 def test_num_frames():
     from diffractometrics import number_frames
     path = ds.cfg.PATH_DIR_H5 / '1149_2-Lysozyme_5_master.h5'
     n = number_frames(path)
-    print(n)
+    print('number of frames:', n)
+    assert isinstance(n, int)
+    assert n > 0
 
 
-if __name__ == '__main__':
-    test_extract_ys()
+def test_signal_strength():
+    from diffractometrics import signal_strength
+    cbfpath = ds.cfg.PATH_DIR_CBF / '1149_2-Lysozyme_5_master.h5' / 'out000277.cbf'
+    ss = signal_strength(cbfpath)
+    assert isinstance(ss, int)
