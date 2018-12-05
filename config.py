dd3c80a85b16da5534d74b130256e1a0f0bb9aff
Jonathan Schurmann <jonathan.schurmann@gmail.com>
2018-12-05 13:52:34 +0100

    added SNAPSHOT_DIR in config

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ PATH_DIR_PROJECT = Path('/mnt/staff/common/ML-crystals/')
 DATA_DIR = PATH_DIR_PROJECT / 'data_pairs'
 PATH_DIR_H5 = PATH_DIR_PROJECT / 'h5'
 PATH_DIR_CBF = PATH_DIR_PROJECT / 'cbf'
+SNAPSHOT_DIR = 'timed_snapshots'
 EIGER_2_CBF = "eiger2cbf"
 SIGNAL_STRENGTH = "distl.signal_strength"
 DIALS_ENV = "/usr/local/dials-v1-11-4/dials_env.sh"
